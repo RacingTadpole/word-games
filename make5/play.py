aa047dff8c0a9b5ee88a90bad2bc04e1dc8becab
Arthur Street <arthurstreet@yahoo.com>
2021-08-15 07:31:29 +1000

    use get_complete_choices

--- make5/play.py
+++ make5/play.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
-from typing import Iterator
-from make5.utilities import read_frequencies, get_chance, get_subwords
+from make5.utilities import read_frequencies, get_chance, get_subwords, get_complete_choices
 from make5.compile_words import read_words
 from make5.types import FrequencyDict, WordDict
 
@@ -28,38 +26,9 @@ class Choice:
         self.start_index = start_index
         self.padded_word = pad_word(start_index, word, max_length)
         self.subwords = tuple(r[1] for r in get_subwords(words, word))
-        self.score = get_score(word, max_length)
+        self.score = sum(get_score(w, max_length) for w in self.subwords)
         self.chance = 1 - (1 - get_chance(key, word, frequency)) ** (max_length * max_length)  # Improve
         self.weighted_score = self.chance * self.score
-
-
-def get_complete_choices(key: str, words: WordDict) -> Iterator[str]:
-    """
-    >>> from .utilities import _get_test_words
-    >>> get_complete_choices('??og?', _get_test_words())
-    ['blogs', 'clogs', 'slogs', 'adogs', 'blogo', 'clogo', 'slogo']
-    """
-    length = len(key)
-    results = get_subwords(words, key)
-    d = defaultdict(list)
-    for start_index, word in results:
-        d[(start_index, len(word))].append(word)
-    # d[(0, length)] are the solutions so far.
-    for min_length_so_far in range(2, length):
-        for w in d.get((0, min_length_so_far), []):
-            for start_index in range(1, length - 1):
-                for comparison_length in range(min_length_so_far - start_index + 1, length):
-                    for w2 in d.get((start_index, comparison_length), []):
-                        # print((0, min_length_so_far, w), (start_index, comparison_length, w2), list(range(start_index, min_length_so_far)))
-                        if all(w[i] == w2[i - start_index] for i in range(start_index, min_length_so_far)):
-                            # These two words match at all common positions.
-                            # If it's a new combination, add it to the dictionary d.
-                            new_combo = w + w2[min_length_so_far - 1:]
-                            new_key = (0, len(new_combo))
-                            if new_combo not in d[new_key]:
-                                # print(new_key, new_combo)
-                                d[new_key].append(new_combo)
-    return d[(0, length)]
 
 
 if __name__ == '__main__':
@@ -73,8 +42,8 @@ if __name__ == '__main__':
             print('Goodbye!')
             exit()
         
-        results = get_subwords(words, key)
-        choices = [Choice(word, key, start_index, words, frequency) for start_index, word in results]
+        results = get_complete_choices(key, words)
+        choices = [Choice(word, key, 0, words, frequency) for word in results]
         sorted_choices = sorted(choices, key=lambda s: s.weighted_score)
         for s in sorted_choices:
             print(f'{s.padded_word:5} {s.weighted_score:5.2f} {s.score:4} {(s.chance * 100):6.2f}%: {s.subwords}')
--- make5/utilities.py
+++ make5/utilities.py
@@ -1,5 +1,6 @@
-from make5.types import FrequencyDict, WordDict
+from collections import defaultdict
 from typing import Iterator, Tuple
+from make5.types import FrequencyDict, WordDict
 
 def _get_test_words() -> WordDict:
     """
@@ -28,7 +29,7 @@ def get_subwords(words: WordDict, key: str, min_length: int = 3) -> Iterator[Tup
     >>> list(get_subwords(words, 'skits'))
     [(0, 'ski'), (1, 'kit'), (2, 'its'), (0, 'skit'), (1, 'kits'), (0, 'skits')]
     >>> [z[1] for z in get_subwords(words, '??ogs')]
-    ['ado', 'dog', 'log', 'blog', 'clog', 'slog', 'dogs', 'logs', 'blogs', 'clogs', 'slogs']
+    ['ado', 'too', 'dog', 'log', 'blog', 'clog', 'slog', 'dogs', 'logs', 'blogs', 'clogs', 'slogs']
     """
     yield from (
         (start, w) for start, subkey in get_subsets(key, min_length)
@@ -50,6 +51,37 @@ def get_chance(key: str, word: str, frequency: FrequencyDict, symbol: str = '?')
         if key[i] == symbol:
             x = x * frequency.get(word[i], 1)
     return x
+
+
+def get_complete_choices(key: str, words: WordDict) -> Iterator[str]:
+    """
+    >>> from .utilities import _get_test_words
+    >>> get_complete_choices('??og?', _get_test_words())
+    ['blogs', 'clogs', 'slogs', 'adogs', 'blogo', 'clogo', 'slogo']
+    """
+    # TODO: this is missing the single words which may not fit any others,
+    # eg. 'toog*'.
+    length = len(key)
+    results = get_subwords(words, key)
+    d = defaultdict(list)
+    for start_index, word in results:
+        d[(start_index, len(word))].append(word)
+    # d[(0, length)] are the solutions so far.
+    for min_length_so_far in range(2, length):
+        for w in d.get((0, min_length_so_far), []):
+            for start_index in range(1, length - 1):
+                for comparison_length in range(min_length_so_far - start_index + 1, length):
+                    for w2 in d.get((start_index, comparison_length), []):
+                        # print((0, min_length_so_far, w), (start_index, comparison_length, w2), list(range(start_index, min_length_so_far)))
+                        if all(w[i] == w2[i - start_index] for i in range(start_index, min_length_so_far)):
+                            # These two words match at all common positions.
+                            # If it's a new combination, add it to the dictionary d.
+                            new_combo = w + w2[min_length_so_far - 1:]
+                            new_key = (0, len(new_combo))
+                            if new_combo not in d[new_key]:
+                                # print(new_key, new_combo)
+                                d[new_key].append(new_combo)
+    return d[(0, length)]
 
 
 def read_frequencies(path: str) -> FrequencyDict:
