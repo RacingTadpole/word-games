624d93da5f1f262c155e8350ccf1c4cd9ebd79a2
Arthur Street <arthurstreet@yahoo.com>
2021-08-14 17:04:31 +1000

    add weighted score

--- make5/play.py
+++ make5/play.py
@@ -1,46 +1,16 @@
-from make5.utilities import read_frequencies
-from typing import Iterable, Iterator, Sequence
+from make5.utilities import read_frequencies, get_score, get_chance, get_subwords
 from make5.compile_words import read_words
 from make5.types import FrequencyDict, WordDict
 
 
-def get_subwords(words: WordDict, word: str, min_length: int = 3) -> Iterator[str]:
-    """
-    >>> import os
-    >>> dir_path = os.path.dirname(os.path.realpath(__file__))
-    >>> words = read_words(os.path.join(dir_path, 'test_data.txt'))
-    >>> list(get_subwords(words, 'skits'))
-    ['ski', 'kit', 'its', 'skit', 'kits', 'skits']
-    >>> list(get_subwords(words, 'alogs'))
-    ['log', 'logs']
-    """
-    for length in range(min_length, len(word) + 2):
-        for start_index in range(0, len(word) - length + 1):
-            candidate = word[start_index:start_index + length]
-            if candidate in words:
-                yield candidate
-
-
-def get_score(words: Iterable[str]) -> int:
-    """
-    >>> get_score(['log', 'logs'])
-    7
-    """
-    return sum(len(word) for word in words)
-
-def get_chance(key: str, word: str, frequency: FrequencyDict, symbol: str = '?') -> float:
-    """
-    >>> import os
-    >>> dir_path = os.path.dirname(os.path.realpath(__file__))
-    >>> frequency = read_frequencies(os.path.join(dir_path, 'frequencies.txt'))
-    >>> get_chance('???f?', 'loafs', frequency)
-    9.6e-06
-    """
-    x = 1
-    for i in range(len(word)):
-        if key[i] == symbol:
-            x = x * frequency[word[i]]
-    return x
+class Score:
+    def __init__(self, word: str, key: str, words: WordDict, frequency: FrequencyDict) -> None:
+        self.word = word
+        self.key = key
+        self.subwords = tuple(get_subwords(words, word))
+        self.score = get_score(self.subwords)
+        self.chance = get_chance(key, word, frequency) * 28  # Improve
+        self.weighted_score = self.chance * self.score
 
 
 if __name__ == '__main__':
@@ -53,14 +23,11 @@ if __name__ == '__main__':
             print('Goodbye!')
             exit()
         
-        results = words[key]
+        results = words.get(key, [])
         # TODO: allow non-5-letter words too
-        scores = [{
-            'subwords': tuple(get_subwords(words, result)),
-            'chance': get_chance(key, result, frequency),
-            'word': result,
-        } for result in results]
-        sorted_scores = sorted(scores, key=lambda x: get_score(x['subwords']))
+        scores = [Score(word, key, words, frequency) for word in results]
+        sorted_scores = sorted(scores, key=lambda s: s.score)
         for s in sorted_scores:
-            print(f'{s["word"]:5} {get_score(s["subwords"]):4} {(s["chance"] * 100):.4f}%: {s["subwords"]}')
+            print(f'{s.word:5} {get_score(s.subwords):4} {(s.chance * 100):5.1f}%: {s.subwords}')
+        print(f'Total weighted score: {sum(s.weighted_score for s in scores):5.1f}')
         print()
--- make5/utilities.py
+++ make5/utilities.py
@@ -1,6 +1,6 @@
 import itertools
-from make5.types import FrequencyDict
-from typing import Iterator
+from make5.types import FrequencyDict, WordDict
+from typing import Iterable, Iterator
 
 
 def get_words_with_letters_missing(word: str, symbol: str = '?') -> Iterator[str]:
@@ -12,6 +12,46 @@ def get_words_with_letters_missing(word: str, symbol: str = '?') -> Iterator[str
         for replacement_indexes in itertools.combinations(range(len(word)), r):
             result = ''.join(symbol if i in replacement_indexes else word[i] for i in range(len(word)))
             yield result
+
+
+def get_subwords(words: WordDict, word: str, min_length: int = 3) -> Iterator[str]:
+    """
+    >>> import os
+    >>> dir_path = os.path.dirname(os.path.realpath(__file__))
+    >>> words = read_words(os.path.join(dir_path, 'test_data.txt'))
+    >>> list(get_subwords(words, 'skits'))
+    ['ski', 'kit', 'its', 'skit', 'kits', 'skits']
+    >>> list(get_subwords(words, 'alogs'))
+    ['log', 'logs']
+    """
+    for length in range(min_length, len(word) + 2):
+        for start_index in range(0, len(word) - length + 1):
+            candidate = word[start_index:start_index + length]
+            if candidate in words:
+                yield candidate
+
+
+def get_score(words: Iterable[str]) -> int:
+    """
+    >>> get_score(['log', 'logs'])
+    7
+    """
+    return sum(len(word) for word in words)
+
+
+def get_chance(key: str, word: str, frequency: FrequencyDict, symbol: str = '?') -> float:
+    """
+    >>> import os
+    >>> dir_path = os.path.dirname(os.path.realpath(__file__))
+    >>> frequency = read_frequencies(os.path.join(dir_path, 'frequencies.txt'))
+    >>> get_chance('???f?', 'loafs', frequency)
+    9.6e-06
+    """
+    x = 1
+    for i in range(len(word)):
+        if key[i] == symbol:
+            x = x * frequency[word[i]]
+    return x
 
 
 def read_frequencies(path: str) -> FrequencyDict:
